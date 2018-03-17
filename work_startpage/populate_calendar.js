f3b270faf05228694a86a65bc2aa5a400ce7ed56
PoopLord1 <tylerbarrett2010@gmail.com>
2018-03-17 03:07:48 -0400

    Set up the CSS for the monthly calendar, and created an empty file for populating it.

---
+++ work_startpage/populate_calendar.js
@@ -0,0 +1,10 @@
+function populate_calendar() {
+	// Find the current month
+	// Find the day of the week the current month starts on
+	// Populate with the appropriate number of "ghost" days
+	// Then find the right number of days in the month
+	// Count up to that number, adding a new day each time
+	// Add the "today" class when you find today's date
+}
+
+populate_calendar();
